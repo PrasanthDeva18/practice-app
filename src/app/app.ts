3ab6f663f199c522d71666af7c2ab99760cc4fc4
prasanthdeva18 <prasanthdeva18@gmail.com>
2025-11-03 14:28:53 +0530

    learned angular to create comp and button click events

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Login } from './login/login';
+import { ProfileComponent } from './profile/profile';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Login, ProfileComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('practice-app');
+  protected readonly title = signal('learning-angular');
 }
